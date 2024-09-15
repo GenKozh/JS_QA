463ae2431f2100e692c16b7dd89935bbcdef5d6b
GenKozh <gen.kozh@gmail.com>
2024-09-15 21:16:42 +0300

    fourth task

---
+++ HW1/Taks4/script.js
@@ -0,0 +1,11 @@
+// Creating an object with various data types as properties
+const myObject = {
+  string: "Hello, World!",       // String
+  number: 42,                    // Number
+  boolean: true,                 // Boolean
+  undefined: undefined,          // Undefined
+  null: null                     // Null
+};
+
+// Displaying the object in the console to verify
+console.log(myObject);
