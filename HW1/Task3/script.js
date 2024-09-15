f52da7fb9cc5cb626c4cdfc15a574ed2c7783308
GenKozh <gen.kozh@gmail.com>
2024-09-15 21:07:00 +0300

    third task

---
+++ HW1/Task3/script.js
@@ -0,0 +1,18 @@
+// a) Declare two variables
+let variable1;
+let variable2;
+
+// b) Assign values to the variables
+variable1 = "Hello";
+variable2 = "World";
+
+// c) Display the values of the variables on the screen
+alert("variable1: " + variable1);
+alert("variable2: " + variable2);
+
+// d) Copy the value of one variable to another
+variable2 = variable1;
+
+// e) Display the values of the variables on the screen
+alert("After copying, variable1: " + variable1);
+alert("After copying, variable2: " + variable2);
