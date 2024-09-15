09d154cb7de26108bf1f65959a47433845adaaff
GenKozh <gen.kozh@gmail.com>
2024-09-15 22:07:48 +0300

    seventh task

---
+++ HW1/Task7/script.js
@@ -0,0 +1,7 @@
+// Ask the user for login, email, and password using prompt()
+const login = prompt("Enter your login:");
+const email = prompt("Enter your email:");
+const password = prompt("Enter your password:");
+
+// Display a message with the entered data
+alert(`Dear ${login}, your email is ${email}, your password is ${password}.`);
