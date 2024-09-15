fada02f8670bdbf867d6c4431b1f6c2dfaf5e2be
GenKozh <gen.kozh@gmail.com>
2024-09-15 21:19:20 +0300

    fifth task

---
+++ HW1/Task5/script.js
@@ -0,0 +1,5 @@
+// Ask the user if they have reached the age of majority
+const isAdult = confirm("Have you reached the age of majority?");
+
+// Output the result to the console
+console.log(isAdult);
