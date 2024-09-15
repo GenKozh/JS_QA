41bcaf809dc1994c1995c48c65325f635f89dd2a
GenKozh <gen.kozh@gmail.com>
2024-09-15 21:23:59 +0300

    sixth task

---
+++ HW1/Task6/script.js
@@ -0,0 +1,30 @@
+// Declaring and assigning variables
+let firstName = "Gennadiy";             // Your name
+let lastName = "Kozhushok";               // Your last name
+let studyGroup = "JS QA";         // Study group
+let birthYear = 1977;               // Year of birth
+
+// Boolean variable for marital status
+let isMarried = false;              // Marital status
+
+// Determine the type of each variable
+console.log(typeof birthYear);      // Number
+console.log(typeof isMarried);      // Boolean
+console.log(typeof firstName);      // String
+console.log(typeof lastName);       // String
+console.log(typeof studyGroup);     // String
+
+// Display variable values in the console in the order: Number, Boolean, String
+console.log(birthYear);
+console.log(isMarried);
+console.log(firstName);
+console.log(lastName);
+console.log(studyGroup);
+
+// Create Null and Undefined variables
+let someNull = null;                // Null variable
+let someUndefined;                  // Undefined variable
+
+// Output the type of Null and Undefined variables
+console.log(typeof someNull);       // Should output "object" (special case in JavaScript)
+console.log(typeof someUndefined);  // Should output "undefined"
